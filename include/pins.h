68f80ff060504108cfeeb5e1c5a39bec0d4343f5
Ayke van Laethem <aykevanlaethem@gmail.com>
2022-03-24 01:45:58 +0100

    Update pins to what I've actually soldered

--- include/pins.h
+++ include/pins.h
@@ -1,7 +1,7 @@
 // Define which hardware pin to use for what.
 // Note: not all pins are usable!
 
-#define LED_PIN       23
-#define ROTARY_BUTTON 22
-#define ROTARY_PIN_A  5
-#define ROTARY_PIN_B  4
+#define LED_PIN       13
+#define ROTARY_BUTTON 25
+#define ROTARY_PIN_A  32
+#define ROTARY_PIN_B  33
